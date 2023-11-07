2a81cdd313f2f2ddc9ce5260c5dd9b27333d2058
0swag <149964801+0swag@users.noreply.github.com>
2023-11-07 20:14:42 +0000

    Update main.cs
    Added support for multiple extensions; Better error description

--- main.cs
+++ main.cs
@@ -2,40 +2,57 @@ using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading.Tasks.Dataflow;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        Console.WriteLine("Enter webhook: ");
-        string webhook_url = Console.ReadLine();
+        Console.WriteLine("Enter webhook URL (or multiple webhook URLs separated by a space): ");
+        string[] webhooks = Console.ReadLine().Split(' ');
+
         Console.WriteLine("Message to spam: ");
         string message = Console.ReadLine();
+
         Console.WriteLine("How many times to spam: ");
         int repeat = Convert.ToInt32(Console.ReadLine());
 
-        var tasks = Enumerable.Range(0, repeat)
-            .Select(async i =>
+        int sent_times = 0;
+
+        var tasks = new List<Task>();
+
+        for (int i = 0; i < repeat; i++)
+        {
+            tasks.AddRange(webhooks.Select(async webhook_url =>
             {
                 using (var http_client = new HttpClient())
                 {
-                    var json_payload = $"{{ \"content\": \"{message}\" }}";
-                    var http_content = new StringContent(json_payload, Encoding.UTF8, "application/json");
-                    HttpResponseMessage http_response = await http_client.PostAsync(webhook_url, http_content);
-                    if (http_response.IsSuccessStatusCode)
+                    bool sent = false;
+                    while (!sent)
                     {
-                        Console.WriteLine("[+] Message Sent!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("[!] Failed to Send Message. Retrying..");
+                        var json_payload = $"{{\"content\": \"{message}\"}}";
+                        var http_content = new StringContent(json_payload, Encoding.UTF8, "application/json");
+
+                        var http_response = await http_client.PostAsync(webhook_url, http_content);
+
+                        if (http_response.IsSuccessStatusCode)
+                        {
+                            sent_times++;
+                            sent = true;
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            Console.WriteLine("[+] Sent message.");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("[!] Failed to send message because of rate limiting or the webhook got deleted. Retrying...");
+                            await Task.Delay(10000);
+                        }
                     }
                 }
-            })
-            .ToArray();
+            }));
+        }
 
         await Task.WhenAll(tasks);
     }
